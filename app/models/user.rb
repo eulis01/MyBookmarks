462e64ebf21835b31498e1f356d7477b30290d8e
Eulis Bonifacio <eulis01@gmail.com>
2021-08-17 16:57:44 -0400

    add dependent: :destroy to has many of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   # Using Bcrypt gem for encryption
   has_secure_password 
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :destroy
   has_many :folders, through: :bookmarks 
 
 
-  has_many :tags
+  has_many :tags, dependent: :destroy
   has_many :tagged_bookmarks, through: :tags, source: :bookmark
 
 
