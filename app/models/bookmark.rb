824e4f9704e72105250af8a4a6d053598a863154
Eulis Bonifacio <eulis01@gmail.com>
2021-08-17 00:12:15 -0400

    add relationship  for user:tag:bookmark_folder

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
   belongs_to :folder
+  has_many :tags
+  has_many :users, through: :tags
+  
+  has_many :bookmark_folders, through: :folder
 end
