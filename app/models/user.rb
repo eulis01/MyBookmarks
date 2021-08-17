58cfc3e2089f8b5db9e099bf4e9f222dbd2a83d8
Eulis Bonifacio <eulis01@gmail.com>
2021-08-17 00:00:35 -0400

    added relationship to bookmarks/folders/tags & minimal validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  # Use Bcrypt for encryption
+  has_secure_password 
+  has_many :bookmarks
+  has_many :folders, through: :bookmarks 
+
+
+  has_many :tags
+  has_many :tagged_bookmarks, through: :tags, source: :bookmark
+
+
+  with_options presence: true do
+    validates :username, :email, uniqueness: true
+  end
+
+  
 end
