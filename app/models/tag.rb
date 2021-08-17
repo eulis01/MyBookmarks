86be2ee6a104be845d21ac29bb0829c9794dbda0
Eulis Bonifacio <eulis01@gmail.com>
2021-08-17 00:06:20 -0400

    add presence: true to bookmark_counts validation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,9 +1,10 @@
 class Tag < ApplicationRecord
-  belongs_to :bookmark
-  belongs_to :user
+  belongs_to :bookmark 
+  belongs_to :user 
 
   with_options presence: true do 
     validates :name, uniqueness: true
-    validates :bookmark_counts
+    validates :bookmark_counts, presence: true
   end
+  
 end
