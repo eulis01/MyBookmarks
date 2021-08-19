bc388986c181996609daca1a7c315b94e30f5365
Eulis Bonifacio <eulis01@gmail.com>
2021-08-18 23:17:05 -0400

    added sessions controller logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,37 @@
 class SessionsController < ApplicationController
   def home 
+
   end
 
+  def new
+
+  end
+
+  def create
+    user = User.find_by(username: params[:username]) 
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:error] = "Invalid username or password"
+      redirect_to "/login"
+  end
   
+  def github
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+    self.current_user = @user
+    redirect_to root_path
+  end
+
+  def failure
+    flash[:error] = "Invalid username or password"
+    redirect_to "/login"
+  end
+
+  def destroy
+        session[:user_id] = nil
+    redirect_to root_path
+  end
+  end
 
 end
