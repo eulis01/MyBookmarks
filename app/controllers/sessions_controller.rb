c21b27b5efb08a74a55a89a4a15c2a6fed90a692
Eulis Bonifacio <eulis01@gmail.com>
2021-08-19 23:35:37 -0400

    comment out code testing abstraction

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,40 +1,48 @@
 class SessionsController < ApplicationController
+skip_before_action :verify_authenticity_token, only: :create
   def home 
   end
 
-  def new
-  end
-
   def create
-    user = User.find_by(username: params[:username]) 
-    if user && user.authenticate(params[:user][:password])
-      session[:user_id] = user.id
-      redirect_to user_path(user)
+    if auth
+      byebug
     else
-      flash[:error] = "Invalid username or password"
-      redirect_to login_path
-    end
-  end
-  
-  def github
-    auth_hash = request.env['omniauth.auth']
-    @user = User.find_or_create_by(email:auth_hash[:info][:email]) do |user|
-      user.username = auth_hash[:info][:username]
-      user.password = SecureRandom.hex(32)
-    end
-    if @user.save
-      session[:user_id] = user.id
-      flash[:success] = "Welcome to the site, #{user.username}"
-    redirect_to user_path(@user)
-    else
-      redirect_if_not_logged_in
+
     end
   end
 
-  def failure
-    flash[:error] = "Invalid username or password"
-    redirect_to login_path
-  end
+  # def create
+  #   user = User.find_by(username: params[:username]) 
+  #   if user && user.authenticate(params[:user][:password])
+  #     session[:user_id] = user.id
+  #     redirect_to user_path(user)
+  #   else
+  #     flash[:error] = "Invalid username or password"
+  #     redirect_to login_path
+  #   end
+  # end
+  
+  # def github
+    
+  #   @user = User.find_or_create_by(email:auth[:info][:email]) do |user|
+  #     user.email = auth[:info][:email]
+  #     user.username = auth[:info][:username]
+  #     user.uid = auth[:info][:uid]
+  #     user.password = SecureRandom.hex(32)
+  #   end
+  #   if @user.save
+  #     session[:user_id] = user.id
+  #     flash[:success] = "Welcome to the site, #{user.username}"
+  #   redirect_to user_path(@user)
+  #   else
+  #     redirect_if_not_logged_in
+  #   end
+  # end
+
+  # def failure
+  #   flash[:error] = "Invalid username or password"
+  #   redirect_to login_path
+  # end
 
   def destroy
         session[:user_id] = nil
@@ -42,6 +50,7 @@ class SessionsController < ApplicationController
   end
 
   private
+
   def auth
     request.env['omniauth.auth']
   end
