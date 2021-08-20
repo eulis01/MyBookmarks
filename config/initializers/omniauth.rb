7d3d74d95a1c3ede30d6685979243daed4739893
Eulis Bonifacio <eulis01@gmail.com>
2021-08-19 23:32:23 -0400

    add comment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  # Using Rails build in credentials.yml.enc file for encrypted credentials.
+  provider :developer unless Rails.env.production?
   provider :github, Rails.application.credentials.github[:key], Rails.application.credentials.github[:secret]
 end
