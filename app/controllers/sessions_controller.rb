3f4074fbdee1224b1ecd8f51db7f8f1a2e9de549
Eulis Bonifacio <eulis01@gmail.com>
2021-08-23 15:22:38 -0400

    session controller login logic fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,47 @@
 class SessionsController < ApplicationController
-skip_before_action :verify_authenticity_token, only: :create
   def home 
   end
 
   def create
     if auth
-      byebug
+      @user = User.find_or_create_by(email:auth[:info][:email]) do |user|
+        user.username = auth[:info][:nickname]
+        user.email = auth[:info][:email]
+        user.uid = auth[:uid]
+        user.password = SecureRandom.hex(16)
+      end
+      if @user.save
+        session[:user_id] = @user.id
+        flash[:success] = "Welcome,#{@user.username}"
+        redirect_to user_path(@user)
+      else
+        flash[:error] = "!!!There was an error creating a User!!!"
+        redirect_if_not_logged_in
+      end
     else
-
+      @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = user.id
+        flash[:success] = "Welcome,#{@user.username}"
+        redirect_to user_path(@user)
+      else
+        flash[:error] = "Invalid username or password"
+        redirect_to login_path
+      end
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+  
+  private
+  
+  def auth
+    request.env['omniauth.auth']
+  end
+end
 
   # def create
   #   user = User.find_by(username: params[:username]) 
@@ -43,15 +75,3 @@ skip_before_action :verify_authenticity_token, only: :create
   #   flash[:error] = "Invalid username or password"
   #   redirect_to login_path
   # end
-
-  def destroy
-        session[:user_id] = nil
-    redirect_to root_path
-  end
-
-  private
-
-  def auth
-    request.env['omniauth.auth']
-  end
-end
