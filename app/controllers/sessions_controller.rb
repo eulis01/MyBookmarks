b44bb4c124d1a1578b1e08b36146a2bec7210fa0
Eulis Bonifacio <eulis01@gmail.com>
2021-09-12 21:52:26 -0400

    using local method for user and renamed omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,16 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    if params[:provider] == 'github'
-      @user = User.create_by_github_omniauth(auth)
-      session[:user_id] = @user.id
-      flash[:success] = "Welcome,#{@user.username}"
-      redirect_to user_path(@user)
+    if params[:provider] == "github"
+      user = User.find_or_create_from_github_omniauth(auth)
+      if user.persisted?
+        session[:user_id] = user.id
+        flash[:success] = "Welcome,#{user.username}"
+        redirect_to user_path(user)
+      else
+        flash[:error] = "There was an error while trying to authenticate you..."
+        redirect_to root_path
+      end
     else
       @user = User.find_by(email: params[:email])
       if @user && @user.authenticate(params[:user][:password])
