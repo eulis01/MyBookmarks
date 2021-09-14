b1bcb4a71bfa5db2c0c8b8b0bbb0caecf6b03d4a
Eulis Bonifacio <eulis01@gmail.com>
2021-09-14 17:16:20 -0400

    basic folders controller logic still WIP

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,4 +1,24 @@
 class FoldersController < ApplicationController
+  before_action :redirect_if_not_logged_in 
+  
+  def index
+    @folders = current_user.folders
+    @folders = Folder.all
+  end
 
+  def create
+    current_user.folders.create(folder_params)
+    redirect_to folders_path
+  end
 
+  def destroy
+    folder = Folder.find(params[:id])
+    folder.destroy
+    redirect_to folders_path
+  end
+
+private
+  def folder_params
+    params.require(:folder).permit(:name)
+  end
 end
