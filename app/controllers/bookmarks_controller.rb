96dd5ac6cb7de1f229743bd3b205c2f1e763520d
Eulis Bonifacio <eulis01@gmail.com>
2021-09-13 22:41:07 -0400

    added base bookmarkscontroller method/logic

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,51 @@
 class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show
+    set_bookmark
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def create
+    @bookmark = current_user.bookmarks.build(bookmark_params)
+    if @bookmark.save
+      redirect_to @bookmark, notice: 'Bookmark was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @bookmark = Bookmark.find(params[:id])
+    if @bookmark.update(bookmark_params)
+      redirect_to @bookmark, notice: 'Bookmark was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to bookmarks_url, notice: 'Bookmark was successfully destroyed.'
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:name, :url)
+  end
+  
+  def set_bookmark 
+    @bookmark = Bookmark.find(params[:id])
+  end
 end
