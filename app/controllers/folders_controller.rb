db5bf4944379c0835f92608ce6f2e2133deb87a4
Eulis Bonifacio <eulis01@gmail.com>
2021-09-20 14:27:10 -0400

    add before action to set_folder

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,5 +1,6 @@
 class FoldersController < ApplicationController
   before_action :redirect_if_not_logged_in 
+  before_action :set_folder, except: [:index, :new, :create]
   
   def index
     @folders = current_user.folders
