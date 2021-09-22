d29ddfba0f3da91a9549ebb1d9ff2c329ab36d5e
Eulis Bonifacio <eulis01@gmail.com>
2021-09-22 15:06:21 -0400

    removed :folder foreing key

--- db/migrate/20210812131235_create_bookmarks.rb
+++ db/migrate/20210812131235_create_bookmarks.rb
@@ -4,7 +4,6 @@ class CreateBookmarks < ActiveRecord::Migration[6.1]
       t.string :name
       t.string :url
       t.belongs_to :user, null: false, foreign_key: true
-      t.belongs_to :folder, null: false, foreign_key: true
 
       t.timestamps
     end
