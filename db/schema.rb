ca2a51d33e2b509a8ad6a341fa5b4002f18a4222
Eulis Bonifacio <eulis01@gmail.com>
2021-09-22 15:09:45 -0400

    regenatered database and removed folders

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_133225) do
+ActiveRecord::Schema.define(version: 2021_08_12_133016) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string "name"
     t.string "url"
     t.integer "user_id", null: false
-    t.integer "folder_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["folder_id"], name: "index_bookmarks_on_folder_id"
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
-
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
@@ -44,7 +41,6 @@ ActiveRecord::Schema.define(version: 2021_08_12_133225) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "bookmarks", "folders"
   add_foreign_key "bookmarks", "users"
   add_foreign_key "tags", "bookmarks"
   add_foreign_key "tags", "users"
