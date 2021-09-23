01e026629edcc2b4e0580317e05b9e6eaf2b5859
Eulis Bonifacio <eulis01@gmail.com>
2021-09-23 08:52:35 -0400

    tags controller logic draft

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,14 @@
 class TagsController < ApplicationController
+  
+  def index
+    @tags = Tag.all.includes(:bookmarks)
+  end
+
+  def show
+    @tag = Tag.find_by_id(params[:id])
+  end
+
+  def new
+  end
+
 end
