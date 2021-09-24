b7f2e618af0e647c0ce34219a22891d7cb9cff2e
Eulis Bonifacio <eulis01@gmail.com>
2021-09-23 21:41:01 -0400

    add spacing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,15 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def redirect_if_not_logged_in
-    redirect_to root_path if !logged_in?
-  end
+    def redirect_if_not_logged_in
+      redirect_to root_path if !logged_in?
+    end
 
-  def current_user
-    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-  end
+    def current_user
+      @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
 
-  def logged_in?
-    !!session[:user_id]
-  end
+    def logged_in?
+      !!session[:user_id]
+    end
 end
