b20207b97158203a2e4a2f07579b6e5b396b96cc
Eulis Bonifacio <eulis01@gmail.com>
2021-09-24 11:46:05 -0400

    removed lot of nonsense comment cleanup params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,25 +20,13 @@ class UsersController < ApplicationController
     # this is the user's profile page - security measure to prevent users from seeing other users' profiles.
     redirect_to root_path && session.clear unless current_user.id == params[:id].to_i
     @user = User.find_by_id(params[:id])
-    #@bookmarks = @user.bookmarks
-    #@user = User.includes(bookmarks: :tags).find_by_id(params[:id])
   end
 
   private 
 
   def user_params
-    # this params hash by itself is not secure enough according to the rails docs. - because it allows for mass assignment.
-    #params.require(:user).permit(:username, :email, :uid, :password, :password_confirmation) -- this wasn't working because my params hash was not being nested in the user hash.
-
-    # this way is not secure as it allows for mass assignment.
-        params.permit(:username, :email, :uid, :password, :password_confirmation)
-
-    # this is the most secure way to do it, according to the rails docs.
-      # params.require(:user).permit(:username, :email, :uid, :password, :password_confirmation).tap do |user_params|
-      #  user_params.require(:username, :email, :uid, :password, :password_confirmation)
-      # end
-
-
+    # Allow certain parameters to be passed in at signup.
+    params.permit(:username, :email, :uid, :password, :password_confirmation)
   end
 
   def set_non_auth_uid
