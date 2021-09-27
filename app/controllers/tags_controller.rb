864a9ff5030f0e8776201bc8a301479452e8a12a
Eulis Bonifacio <eulis01@gmail.com>
2021-09-26 21:47:42 -0400

    added tags controller basic actions and logic

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,7 @@
 class TagsController < ApplicationController
+  before_action :redirect_if_not_logged_in
+  before_action :set_tag, only: except: [:index, :new, :create]
+  before_action :set_tag_owner, only: [:edit, :update, :destroy]
   
   def index
     @tags = Tag.all.includes(:bookmarks)
@@ -10,5 +13,32 @@ class TagsController < ApplicationController
 
   def new
   end
+
+  def create 
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def tag_params
+      params.require(:tag).permit(:name)
+    end
+
+    def set_tag
+      @tag = Tag.find_by_id(params[:id])
+    end
+
+    def set_tag_owner
+      set_tag
+      redirect_to tags_path if !@tag || @tag.user_id != current_user.id
+    end
 
 end
