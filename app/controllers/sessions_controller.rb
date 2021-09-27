09d6e588e4f5effcc4807b0b6fd6b0a413ff3c0d
Eulis Bonifacio <eulis01@gmail.com>
2021-09-26 21:47:14 -0400

    add flash message for log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,6 +27,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete(:user_id)
+    flash[:message] = "You have successfully logged out."
     redirect_to root_path
   end
   
