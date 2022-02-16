f754cf8510be6a468b8d94da0c51b11e7277c879
Eulis Bonifacio <eulis01@gmail.com>
2022-02-16 17:18:04 -0500

    move tag methods into helper to clean up a bit

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,2 +1,14 @@
 module TagsHelper
+  def set_tag
+    @tag = Tag.find_by_id(params[:id])
+    if !@tag
+      flash[:message] = "That tag doesn't exist"
+      redirect_to tags_path
+    end
+  end
+
+  def set_tag_owner
+    set_tag
+    redirect_to tags_path if !@tag || @tag.user_id != current_user.id
+  end
 end
