2e02e0202dda365d605b8adca0773c611a8bfbde
Eulis Bonifacio <eulis01@gmail.com>
2022-02-17 16:09:55 -0500

    Merge branch 'wip-tags-controller-and-form'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # For APIs, you may want to use :null_session instead.
   # Give access to helper methods in the view
-  include SessionsHelper
+  include SessionsHelper, BookmarksHelper, TagsHelper
 end
