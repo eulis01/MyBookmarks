ba7e030847c8e6133254d427156e23d23a107fbc
Eulis Bonifacio <eulis01@gmail.com>
2022-02-17 19:22:31 -0500

    added comment with description of code block

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,4 +1,6 @@
 module TagsHelper
+
+  # Find the tag ID and set it to the @tag instance variable if it exists else if not flash an error message.
   def set_tag
     @tag = Tag.find_by_id(params[:id])
     if !@tag
@@ -7,6 +9,7 @@ module TagsHelper
     end
   end
 
+  # First call set_tag an then redirect only if tag instance variable is not set or if current_user user_id is not equal to the user_id of the tag.
   def set_tag_owner
     set_tag
     redirect_to tags_path if !@tag || @tag.user_id != current_user.id
