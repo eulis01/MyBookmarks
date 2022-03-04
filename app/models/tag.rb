f589f06b6d2ca8b33eebe4f9d6049fec7d67e51a
Eulis Bonifacio <eulis01@gmail.com>
2022-03-04 13:44:12 -0500

    added scope to order tags by bookmark count

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,8 @@ class Tag < ApplicationRecord
   belongs_to :bookmark 
   belongs_to :user 
 
+  scope :order_by_bookmark_counts, -> { order(bookmark_counts: :desc) }
+
   with_options presence: true do 
     validates :name, uniqueness: true
   end
