a290a371120b91f51a302c08869ce588fe5e1165
Eulis Bonifacio <eulis01@gmail.com>
2022-03-08 16:30:01 -0500

    custom tags_attribute writter working fine

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,14 +2,14 @@ class Bookmark < ApplicationRecord
   belongs_to :user
   has_many :tags
   has_many :users, through: :tags
-  accepts_nested_attributes_for :tags, allow_destroy: true
+  #accepts_nested_attributes_for :tags, allow_destroy: true
 
-  #def tags_attributes=(tags_attributes)
-  #  tags_attributes.values.each do |tag_attribute|
-  #    tag = Tag.find_or_create_by(tag_attribute)
-  #    self.tags << tag
-  #  end
-  #end
+  def tags_attributes=(tags_attributes)
+    tags_attributes.values.each do |tag_attribute|
+      tag = Tag.find_or_create_by(tag_attribute)
+      self.tags << tag
+    end
+  end
 
     scope :alpha, -> { order(:name)}
 
@@ -19,6 +19,4 @@ class Bookmark < ApplicationRecord
     validates :url, format: { with: /\A#{URI::regexp(%w(http https))}\z/ }, uniqueness: true
     validates :user_id
   end
-
-
 end
